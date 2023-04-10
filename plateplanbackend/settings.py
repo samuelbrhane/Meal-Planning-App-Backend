c1e4fab49110c2f0a9b7b965cf79f5c1ef05e2b3
samuelberhane <1samibrhane@gmail.com>
2023-04-10 13:25:45 +0300

    modify the urls and models for google auth authentication.

--- plateplanbackend/settings.py
+++ plateplanbackend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -163,7 +164,7 @@ DJOSER = {
     "SERIALIZERS": {
         "user_create":"account.serializers.UserCreateSerializer",
         "user":"account.serializers.UserCreateSerializer",
-        "current_user":"account.serializers.UserCreateSerializer",
+        # "current_user":"account.serializers.UserCreateSerializer",
         "user_delete":"account.serializers.UserDeleteSerializer",
     }
 }
--- plateplanbackend/urls.py
+++ plateplanbackend/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
 ]
 
 urlpatterns += [re_path(r'^.*',TemplateView.as_view(template_name="index.html"))]
