77fda549dd244b3fc24c6b3676cc648daa203496
samuelberhane <1samibrhane@gmail.com>
2023-04-12 11:41:56 +0300

    Add media file to store public image files.

--- plateplanbackend/settings.py
+++ plateplanbackend/settings.py
@@ -142,11 +142,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'assets/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "dist/assets/"),
+    BASE_DIR / "dist/assets",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "dist/")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+FILE_UPLOAD_PERMISSIONS = 0o640
+
 
 DJOSER = {
     "LOGIN_FIELD": "email",
@@ -179,7 +185,6 @@ SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
    "ACCESS_TOKEN_LIFETIME": timedelta(hours=6),
    "REFRESH_TOKEN_LIFETIME": timedelta(hours=12),
-
 }
 
 
@@ -191,6 +196,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "authentication.UserAccount"
 
 CORS_ORIGIN_ALLOW_ALL = True
+
 
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:5173', 
--- plateplanbackend/urls.py
+++ plateplanbackend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,re_path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -24,6 +26,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.social.urls')),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [re_path(r'^.*',TemplateView.as_view(template_name="index.html"))]
